47f930e45df0934b4ddcc2156d4747b176629a88
AngeCk98 <Angekoubangou@gmail.com>
2022-10-13 00:39:42 -0700

    6-size.c file

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+/**
+ *
+ *  * main-prints the outcome of the printf function
+ *
+ *   *
+ *
+ *    * Description: using the main function
+ *
+ *     * this program prints the size of various data types and their sizes
+ *
+ *      * Return:0
+ *
+ *      */
+
+int main(void)
+
+{
+
+		char c;
+
+			int i;
+
+				long long_i;
+
+					long long long_long_i;
+
+						float f;
+
+
+
+							printf("Size of a char: %ld byte(s)\n", sizeof(c));
+
+								printf("Size of an int: %ld byte(s)\n", sizeof(i));
+
+									printf("Size of a long int: %ld byte(s)\n", sizeof(long_i));
+
+										printf("Size of a long long int: %ld byte(s)\n", sizeof(long_long_i));
+
+											printf("Size of a float: %ld byte(s)\n", sizeof(f));
+
+												return (0);
+
+}
