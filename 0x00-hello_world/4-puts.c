c0c2042e8e282fdef621c4d116b4ed215add2e2a
AngeCk98 <Angekoubangou@gmail.com>
2022-10-13 00:28:41 -0700

    4-puts.c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - prints the string using the puts funtion
+ *
+ *   *
+ *
+ *    * Description:using the main function
+ *
+ *     * this code prints a 'Programming is like building a multilingual puzzle'
+ *
+ *      * Return:0
+ *
+ *      */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+
+
+}
