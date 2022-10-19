47fc005e7a25b944a9bae4504475417267d66e61
AngeCk98 <Angekoubangou@gmail.com>
2022-10-18 23:59:49 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,44 +1,49 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * print_times_table - n times table.
- * @n: the number defines the table.
- *
- * Return: no return value
+ * print_times_table - Print the n times table, starting with 0
+ * @n: int type; collects range of timetable
+ * Return: 0
  */
 void print_times_table(int n)
 {
-	int num, mult, prod;
-
-	if (n >= 0 && n <= 15)
-	{
-		for (num = 0; num <= n; num++)
-		{
-			_putchar('0');
-
-			for (mult = 1; mult <= n; mult++)
-			{
-				_putchar(',');
-				_putchar(' ');
-
-				prod = num * mult;
-
-				if (prod <= 99)
-					_putchar(' ');
-				if (prod <= 9)
-					_putchar(' ');
-
-				if (prod >= 100)
-				{
-					_putchar((prod / 100) + '0');
-					_putchar(((prod / 10)) % 10 + '0');
-				}
-				else if (prod <= 99 && prod >= 10)
-				{
-					_putchar((prod / 10) + '0');
-				}
-				_putchar((prod % 10) + '0');
-			}
-			_putchar('\n');
-		}
-	}
+int x = 0, y, z;
+if (n > 15 || n < 0)
+return;
+while (x <= n)
+{
+for (y = 0; y <= n; y++)
+{
+z = x * y;
+if (z > 99)
+{
+putchar(z / 100 + '0');
+putchar((z / 10 % 10) + '0');
+putchar(z % 10 + '0');
 }
+else if (z > 9)
+{
+putchar(' ');
+putchar(z / 10 + '0');
+putchar(z % 10 + '0');
+}
+else if (y != 0)
+{
+putchar(' ');
+putchar(' ');
+putchar(z + '0');
+}
+else
+putchar(z + '0');
+if (y != n)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+x++;
+}
+}
+
+
