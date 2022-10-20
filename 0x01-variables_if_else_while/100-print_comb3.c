95d5b1840566c3ba00c66202020b307bf7800703
AngeCk98 <Angekoubangou@gmail.com>
2022-10-20 09:38:51 -0700

    100-print_comb3.c hot fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,89 +1,61 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 /**
+ * main - mainn block
  *
- *  * main - mainn block
+ * Description: Write a program that prints all possible
  *
- *   * Description: Write a program that prints all possible
+ * different combinations of two digits
  *
- *     * different combinations of two digits
+ * Numbers must be separated by ,, followed by a space
  *
- *       * Numbers must be separated by ,, followed by a space
+ * The two digits must be different
  *
- *         * The two digits must be different
+ * 01 and 10 are considered the same combination of the two digits 0 and 1
  *
- *           * 01 and 10 are considered the same combination of the two digits 0 and 1
+ * Print only the smallest combination of two digits
  *
- *             * Print only the smallest combination of two digits
+ * Numbers should be printed in ascending order, with two digits
  *
- *               * Numbers should be printed in ascending order, with two digits
+ * You can only use the putchar function
  *
- *                 * You can only use the putchar function
+ * (every other function (printf, puts, etc…) is forbidden)
  *
- *                   * (every other function (printf, puts, etc…) is forbidden)
+ * You can only use putchar five times maximum in your code
  *
- *                     * You can only use putchar five times maximum in your code
+ * You are not allowed to use any variable of type char
  *
- *                       * You are not allowed to use any variable of type char
+ * All your code should be in the main function
  *
- *                         * All your code should be in the main function
+ * Return: 0
  *
- *                           * Return: 0
- *
- *                             */
+ */
 
 int main(void)
-
 {
+	int c;
+	int d = 0;
 
-		int c;
+	while (d < 10)
+	{c = 0;
+	while (c < 10)
+	{
+	if (d != c && d < c)
+	{
+		putchar('0' + d);
+		putchar('0' + c);
 
-			int d = 0;
-
-
-
-				while (d < 10)
-
-						{c = 0;
-
-									while (c < 10)
-
-												{
-
-																if (d != c && d < c)
-
-																				{
-
-																									putchar('0' + d);
-
-																													putchar('0' + c);
-
-
-
-																																	if (c + d != 17)
-
-																																						{
-
-																																												putchar(',');
-
-																																																	putchar(' ');
-
-																																																					}
-
-																																				}
-
-																			c++;
-
-																					}
-
-											d++;
-
-												}
-
-					putchar('\n');
-
-						return (0);
-
+		if (c + d != 17)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	c++;
+	}
+	d++;
+	}
+	putchar('\n');
+	return (0);
 }
+
