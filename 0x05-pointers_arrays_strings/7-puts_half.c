379a03aa902c0886ca1b8c784726c52147b7134f
AngeCk98 <Angekoubangou@gmail.com>
2022-10-24 01:04:05 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * puts_half - outputs half of string
+ * @str: collect whole string
+ */
+void puts_half(char *str)
+{
+	int len, n, a;
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	if (len % 2 == 0)
+	{
+	for (a = len / 2; str[a] != '\0'; a++)
+	{
+	
+		putchar(str[a]);
+	}
+	}
+	else if (len % 2)
+	{
+	for (n = (len - 1) / 2; n < len - 1; n++)
+	{
+		putchar(str[n + 1]);
+	}
+	}
+	putchar('\n');
+}
+
