0a05e1c38e3ca46494c4ecbade8eb6fd051b426a
AngeCk98 <Angekoubangou@gmail.com>
2022-10-24 00:56:26 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * puts2 - outputs string
+ * @str: collect string
+ */
+void puts2(char *str)
+{
+	int length = strlen(str);
+	int i;
+
+	for (i = 0; i < length ; i++)
+	{
+		if (i % 2 == 0)
+
+		putchar(str[i]);
+	}
+	printf("\n");
+}
