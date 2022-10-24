204860ec3fcba59b7c1d776c9cb721af61523944
AngeCk98 <Angekoubangou@gmail.com>
2022-10-24 00:41:18 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * print_rev - outputs reverse of a string
+ * @s: collect string
+ */
+void print_rev(char s[])
+{
+	int length = strlen(s);
+
+	int i;
+
+	for (i = length - 1; i >= 0 ; i--)
+	{
+		putchar(s[i]);
+	}
+	printf("\n");
+}
