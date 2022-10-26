82b3401e74ff8efbfd08f3d4a867b07e7735f380
AngeCk98 <Angekoubangou@gmail.com>
2022-10-26 00:19:51 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * string_toupper - Converts lowercase to uppercase
+ * @c: collects string in lowercase
+ * Return: converted string
+ */
+char *string_toupper(char *c)
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+
+	{
+
+		if (c[i] > 96 && c[i] < 123)
+
+		{
+
+			c[i] -= 32;
+
+		}
+
+	}
+
+	return (c);
+
+}
